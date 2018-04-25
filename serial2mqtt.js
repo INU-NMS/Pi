bccc5a3813b663558f883ce7c748fe7fa5849dcf
NMS00X <NMS00X@gmail.com>
2018-04-25 12:30:57 +0000

    issue001(mqtt 중복문제) fix

--- serial2mqtt.js
+++ serial2mqtt.js
@@ -1,5 +1,5 @@
 const s = require('serialport')
-const mqtt = require('mqtt').connect('mqtt://nms.iptime.org:8080')
+const mqtt = require('mqtt').connect('mqtt://nms.iptime.org:23')
 const util = require('util');
 
 const serial = new s('/dev/ttyACM0', { baudRate: 115200 })
@@ -8,16 +8,24 @@ const parser = new s.parsers.Readline({ delimiter: '\n' })
 var _eui;
 var _topic;
 
+var is_rcnt = false;
+
 serial.on('open', () => {
 	console.log('serial2mqtt now running')
 	serial.pipe(parser)
 
 	mqtt.on('connect', () => {
-		console.log('connected to the mqtt server')
-		mqtt.subscribe('node/#')
+		if(is_rcnt) {
+			console.log('reconnected to the mqtt server');
+			return;
+		} else {
+			is_rcnt = true;
+			console.log('connected to the mqtt server');
+			mqtt.subscribe('node/#')
+		}
 
 		parser.on('data', (data) => {
-			console.log('[Serial] <<', String(data))
+			console.log('[serial]: ', String(data))
 			if(data.includes('TX DONE')) mqtt.publish(_topic, 'TX DONE');
 			if(data.includes('0080')) {
 				_eui = data.replace(/(.{2})/g, "$1-").slice(0, -2);
@@ -30,9 +38,9 @@ serial.on('open', () => {
 		})
 
 		mqtt.on('message', (topic, payload) => {
-			if(!topic.include('all') || !topic.include(_eui)) return;
+			if(!(topic.includes('all') || topic.includes(_eui))) return;
 			if(!topic.includes('req')) return;
-			console.log(topic, String(payload));
+			console.log('[mqtt]:\t', topic, String(payload));
 
 			if(String(payload) === 'reset') {
 				serial.set({ brk: true })
