d3024e1b47b4b6db1f91b66a9bc4e7f8a390f957
NMS00X <NMS00X.gmail.com>
2018-04-28 12:17:21 +0900

    issue#1 mDot 연결 테스트 남음

--- serial2mqtt.js
+++ serial2mqtt.js
@@ -1,43 +1,39 @@
 const DEV_PORT = '/dev/ttyACM0';
 const BROKER_ADDR = 'nms.iptime.org:23';
 
+const udev = require('udev');
 const s = require('serialport');
 const mqtt = require('mqtt').connect(`mqtt://${BROKER_ADDR}`);
 
 var eui;
 var isOpened = false;
 var isConnedted = false;
+var monitor = udev.monitor();
 
-const port = new s(DEV_PORT, { baudRate: 115200 });
+const port = new s(DEV_PORT, { baudRate: 115200 }, HandlePortError);
 const parser = new s.parsers.Readline({ delimiter: '\n' })
 
 port.on('open', () => {
-	console.log(`port open: ${DEV_PORT}`);
+	console.log(`[port]\topen ${DEV_PORT}`);
 	isOpened = true;
 	port.pipe(parser);
 })
 
-port.on('error', (err) => {
-	console.log(`port error: ${err}`);
-	isOpened = false;
-	// handling 필요
-})
-
 mqtt.on('connect', () => {
-	console.log(`mqtt connected: ${BROKER_ADDR}`);
+	console.log(`[mqtt]\tconnected ${BROKER_ADDR}`);
 	isConnedted = true;
 	mqtt.subscribe('node/#');
 })
 
 mqtt.on('close', () => {
-	console.log(`mqtt disconnected`);
+	console.log(`[mqtt]\tdisconnected`);
 	isConnedted = false;
 })
 
 parser.on('data', (data) => {
 	if(isConnedted == false) return;
 
-	console.log('[port]:\t', String(data));
+	console.log('[port]\t', String(data));
 	if(data.includes('0080')) {
 		eui = data.replace(/(.{2})/g, "$1-").slice(0, -2);
 		mqtt.publish('node/all/res', _eui);
@@ -47,15 +43,32 @@ parser.on('data', (data) => {
 })
 
 mqtt.on('message', (topic, payload) => {
-	if(isOpened == false) return;
+	if(isOpened == false) {
+		console.log(`[mqtt]\t port is not opened, ignore ${topic}`);
+		return;
+	}
 
 	var isReqTopic = (topic.includes('all') || topic.includes(eui)) && topic.includes('req');
 	if(isReqTopic == false) return;
 
-	console.log('[mqtt]:\t', topic, String(payload));
+	console.log('[mqtt]\t', topic, String(payload));
 	if(String(payload) === 'reset') {
 		port.set({ brk: true })
 		port.set({ brk: false })
 	}
 	port.write(`${String(payload)}\r\n`);
 })
+
+
+monitor.on('add', (dev) => { 
+	console.log(dev);
+	if(dev === DEV_PORT) port.open(HandlePortError); 
+});
+monitor.on('remove', (dev) => { if(dev === DEV_ADDR) console.log(`[port]\tremoved ${DEV_ADDR}`); });
+
+function HandlePortError(err) {
+	console.log(`[port]\t${err.message}`);
+	isOpened = false;
+	console.log(`[port]\twaiting for connection${DEV_PORT}`);
+}
+
